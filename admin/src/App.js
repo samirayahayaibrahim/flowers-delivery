a5eaeb5a528b2a2457badeb9bbf2b618d93ecfb5
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-05-23 04:56:56 -0700

    added admin panel folder with add flower file

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Navbar from './components/Navbar';
+import AddFlowerRoute from './AdminPanel/addFlowerRoute';
+import FlowerDetails from './components/flowerDetails';
+import AddFlowers from "./AdminPanel/addFlowers"
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+            <BrowserRouter>
+        <AddFlowerRoute />
+        <hr/>
+        <div className='pages'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Routes>
+            <Route path="/flowerDetails" element={<AddFlowers />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
     </div>
   );
 }
