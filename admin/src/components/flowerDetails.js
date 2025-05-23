1379026517ce5bbc2c5afce08e9fe8f9d817fb2b
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-05-23 05:16:39 -0700

    added a component folder and a flower detail file

---
+++ admin/src/components/flowerDetails.js
@@ -0,0 +1,37 @@
+import flowerDetails from "./flowerDetails.css";
+import { useFlowersContext } from "../hooks/useFlowersContext";
+
+const FlowerDetails = ({ flower }) => {
+    const { dispatch } = useFlowersContext()
+
+    const handleClick = async() => {
+        const response = await fetch("/api/flowerRoutes/" + flower._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok){
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
+
+    const formattedPrice = `$${Number(flower.price).toFixed(2)}`;
+    return ( 
+        <div className="min">
+        <div className="flowerDetails">
+                <img src={flower.image} /><span id="delete" className="delete" onClick={handleClick}>-</span> 
+                <div className="flowerlist">
+                    <p className="detail">Name:<span className="list">{flower.name}</span></p>
+                    <p className="detail">Category:<span className="list">{flower.category}</span></p>
+                    <p className="detail">Price:<span className="list">{formattedPrice}</span></p>
+                    <p className="detail">Description:<span className="list">{flower.description}</span></p>
+                    <p className="detail"><span className="list">{flower.createdAt}</span></p>
+                </div>
+            
+        </div>
+        </div>
+    )
+}
+
+export default FlowerDetails
+
