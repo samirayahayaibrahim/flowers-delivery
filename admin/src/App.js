6ea4b72779e00610de841c10ae3b1a00564854d8
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-05-23 06:06:07 -0700

    added hook folder and a context folder

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
-import Navbar from './components/Navbar';
 import AddFlowerRoute from './AdminPanel/addFlowerRoute';
 import FlowerDetails from './components/flowerDetails';
 import AddFlowers from "./AdminPanel/addFlowers"
