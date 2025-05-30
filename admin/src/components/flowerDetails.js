5d44074ac3bbd3b0af1f037fe4ff5b26fab42def
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-05-30 09:11:56 -0700

    update styling for admin folder

--- admin/src/components/flowerDetails.js
+++ admin/src/components/flowerDetails.js
@@ -21,13 +21,13 @@ const FlowerDetails = ({ flower }) => {
     return ( 
         <div className="min">
         <div className="flowerDetails">
-                <img src={flower.image}  alt={flower.name}/><span id="delete" className="delete" onClick={handleClick}>-</span> 
+                <img className="img" src={flower.image}  alt={flower.name}/><span id="delete" className="delete" onClick={handleClick}>-</span> 
                 <div className="flowerlist">
                     <p className="detail">Name:<span className="list">{flower.name}</span></p>
                     <p className="detail">Category:<span className="list">{flower.category}</span></p>
                     <p className="detail">Price:<span className="list">{formattedPrice}</span></p>
                     <p className="detail">Description:<span className="list">{flower.description}</span></p>
-                    <p className="detail"><span className="list">{flower.createdAt}</span></p>
+                    
                 </div>
             
         </div>
