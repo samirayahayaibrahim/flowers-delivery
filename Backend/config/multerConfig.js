c3bed80b1c082c1da64246e087c620cb59079c13
Samira Yahaya Ibraheem <samirayahayaibraheem@gmail.com>
2025-11-27 13:25:23 -0800

    update multer

--- Backend/config/multerConfig.js
+++ Backend/config/multerConfig.js
@@ -10,17 +10,30 @@ if (!fs.existsSync(uploadPath)) {
     fs.mkdirSync(uploadPath, { recursive: true });
 }
 
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../../uploads/'));
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "flowers",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
-  filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname).toLowerCase(); // e.g. '.png'
-    const uniqueName = crypto.randomBytes(16).toString('hex') + ext;
-    cb(null, uniqueName);
-  }
 });
+
+
+
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, '../../uploads/'));
+  // },
+//   filename: function (req, file, cb) {
+//     const ext = path.extname(file.originalname).toLowerCase(); // e.g. '.png'
+//     const uniqueName = crypto.randomBytes(16).toString('hex') + ext;
+//     cb(null, uniqueName);
+//   }
+// });
 
 const upload = multer({ storage });
 
