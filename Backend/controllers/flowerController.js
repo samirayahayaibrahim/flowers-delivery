cc58512951edc54adcef3d194a6a0db806bebdb9
Samira Yahaya Ibraheem <samirayahayaibraheem@gmail.com>
2025-11-27 07:12:35 -0800

    update backend

--- Backend/controllers/flowerController.js
+++ Backend/controllers/flowerController.js
@@ -1,116 +1,224 @@
-const Flower = require("../models/flowerModel")
-const mongoose = require('mongoose')
-const multer = require("multer");
-const upload = require("../config/cloudinary");
+// const Flower = require("../models/flowerModel")
+// const mongoose = require('mongoose')
+// const multer = require("multer");
+// const upload = require("../config/cloudinary");
 
 
 
-//all flower
+// //all flower
+// const getFlowers = async (req, res) => {
+//     const flowers = await Flower.find({}).sort({createdAt: -1})
+
+//     res.status(200).json(flowers)
+// }
+
+
+// //single flower
+// const getFlower = async (req, res) => {
+//     const { id } = req.params
+
+//     if (!mongoose.Types.ObjectId.isValid(id)){
+//         return res.status(404).json({error: 'No such flower'})
+//     }
+
+//     const flower = await Flower.findById(id)
+
+//     if (!flower) {
+//     return res.status(404).json({error: 'no such flower'})
+// }
+//     res.status(200).json(flower)
+// }
+
+// // create new flower & connected to db
+// const createFlower = async (req, res) => {
+//     console.log('Incoming form data:', req.body);
+//     console.log('Uploaded file info:', req.file);
+
+
+//     const { name, description, price, category} = req.body
+
+//     let emptyFields = []
+
+//     if(!name) {
+//         emptyFields.push('name')
+//     }
+    
+//     if(!description) {
+//         emptyFields.push('description')
+//     }
+    
+//     if(!price) {
+//         emptyFields.push('price')
+//     }
+    
+//     if(!category) {
+//         emptyFields.push('category')
+//     }
+//     if(emptyFields.length > 0){
+//         return res.status(400).json({ error: 'please fill in all the empty space', emptyFields})
+//     }
+    
+//     if (!req.file) {
+//         return res.status(400).json({ error: 'Image upload failed or missing' });
+//         }
+    
+//     const image = `https://flowers-delivery-backend-jcef.onrender.com/uploads/${req.file.filename}` ;
+    
+//     try{
+//         const flower = await Flower.create({name, image, description, price, category});
+//         res.status(200).json(flower)
+//     } catch (error) {
+//         res.status(400).json({error: error.message})
+
+//     }
+// }
+
+// const deleteFlower = async (req, res) => {
+//     const { id } = req.params
+
+//     if (!mongoose.Types.ObjectId.isValid(id)){
+//         return res.status(404).json({error: 'No such flower'})
+//     }
+
+//     const flower = await Flower.findOneAndDelete({_id: id})
+
+//     if (!flower) {
+//         return res.status(400).json({error: 'no such flower'})
+//     }
+
+//     res.status(200).json(flower)
+// }
+
+// const updateFlower = async (req, res) => {
+//     const { id } = req.params
+
+//     if (!mongoose.Types.ObjectId.isValid(id)){
+//         return res.status(404).json({error: 'No such flower'})
+//     }
+
+//     const flower = await Flower.findOneAndUpdate({_id: id}, {
+//         ...req.body
+//     })
+
+//     if (!flower) {
+//         return res.status(400).json({error: 'no such flower'})
+//     }
+
+//     res.status(200).json(flower)
+// }
+
+// module.exports = {
+//     getFlower,
+//     getFlowers,
+//     createFlower,
+//     deleteFlower,
+//     updateFlower
+// }
+
+
+const Flower = require("../models/flowerModel");
+const mongoose = require("mongoose");
+
+// all flower
 const getFlowers = async (req, res) => {
-    const flowers = await Flower.find({}).sort({createdAt: -1})
+  const flowers = await Flower.find({}).sort({ createdAt: -1 });
+  res.status(200).json(flowers);
+};
 
-    res.status(200).json(flowers)
-}
-
-
-//single flower
+// single flower
 const getFlower = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such flower'})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such flower" });
+  }
 
-    const flower = await Flower.findById(id)
+  const flower = await Flower.findById(id);
 
-    if (!flower) {
-    return res.status(404).json({error: 'no such flower'})
-}
-    res.status(200).json(flower)
-}
+  if (!flower) {
+    return res.status(404).json({ error: "no such flower" });
+  }
+  res.status(200).json(flower);
+};
 
-// create new flower & connected to db
+// create new flower
 const createFlower = async (req, res) => {
-    console.log('Incoming form data:', req.body);
-    console.log('Uploaded file info:', req.file);
+  console.log("Incoming form data:", req.body);
+  console.log("Uploaded file info:", req.file);
 
+  const { name, description, price, category } = req.body;
 
-    const { name, description, price, category} = req.body
+  let emptyFields = [];
 
-    let emptyFields = []
+  if (!name) emptyFields.push("name");
+  if (!description) emptyFields.push("description");
+  if (!price) emptyFields.push("price");
+  if (!category) emptyFields.push("category");
 
-    if(!name) {
-        emptyFields.push('name')
-    }
-    
-    if(!description) {
-        emptyFields.push('description')
-    }
-    
-    if(!price) {
-        emptyFields.push('price')
-    }
-    
-    if(!category) {
-        emptyFields.push('category')
-    }
-    if(emptyFields.length > 0){
-        return res.status(400).json({ error: 'please fill in all the empty space', emptyFields})
-    }
-    
-    if (!req.file) {
-        return res.status(400).json({ error: 'Image upload failed or missing' });
-        }
-    
-    const image = `https://flowers-delivery-backend-jcef.onrender.com/uploads/${req.file.filename}` ;
-    
-    try{
-        const flower = await Flower.create({name, image, description, price, category});
-        res.status(200).json(flower)
-    } catch (error) {
-        res.status(400).json({error: error.message})
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "please fill in all the empty space", emptyFields });
+  }
 
-    }
-}
+  if (!req.file) {
+    return res.status(400).json({ error: "Image upload failed or missing" });
+  }
+
+  // Cloudinary returns the full image URL in req.file.path
+  const image = req.file.path;
+
+  try {
+    const flower = await Flower.create({
+      name,
+      image,
+      description,
+      price,
+      category,
+    });
+
+    res.status(200).json(flower);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 const deleteFlower = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such flower'})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such flower" });
+  }
 
-    const flower = await Flower.findOneAndDelete({_id: id})
+  const flower = await Flower.findOneAndDelete({ _id: id });
 
-    if (!flower) {
-        return res.status(400).json({error: 'no such flower'})
-    }
+  if (!flower) {
+    return res.status(400).json({ error: "no such flower" });
+  }
 
-    res.status(200).json(flower)
-}
+  res.status(200).json(flower);
+};
 
 const updateFlower = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such flower'})
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such flower" });
+  }
 
-    const flower = await Flower.findOneAndUpdate({_id: id}, {
-        ...req.body
-    })
+  const flower = await Flower.findOneAndUpdate({ _id: id }, req.body);
 
-    if (!flower) {
-        return res.status(400).json({error: 'no such flower'})
-    }
+  if (!flower) {
+    return res.status(400).json({ error: "no such flower" });
+  }
 
-    res.status(200).json(flower)
-}
+  res.status(200).json(flower);
+};
 
 module.exports = {
-    getFlower,
-    getFlowers,
-    createFlower,
-    deleteFlower,
-    updateFlower
-}
+  getFlower,
+  getFlowers,
+  createFlower,
+  deleteFlower,
+  updateFlower,
+};
